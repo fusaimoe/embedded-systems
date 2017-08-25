f845069901b3f62bd673f4585e8e1e6fd5a8633e
Andrea Colombo <andread251@gmail.com>
2017-08-26 00:54:50 +0200

    Static web page

---
+++ consegna-4/event_tracker_web/gulpfile.js
@@ -0,0 +1,92 @@
+// Built using the following guide
+// https://css-tricks.com/gulp-for-beginners/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sourcemaps.init())
+      .pipe(sass()) // Converts Sass to CSS with gulp-sass
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    startPath:'./app',
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano({})))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('cache:clear', function (callback) {
+  return cache.clearAll(callback);
+});
+
+// Combined Tasks
+
+gulp.task('default', function (callback) {
+  runSequence( ['sass','browserSync', 'watch'], callback )
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', ['sass', 'useref', 'images', 'fonts'], callback )
+});
+
+gulp.task('css', function (callback) {
+  runSequence('clean:dist', 'sass', 'useref', callback )
+});
---
+++ consegna-4/event_tracker_web/readme.md
@@ -0,0 +1,13 @@
+# gulp-web-base
+
+A boilerplate I use to create static sites with Gulp.
+
+## To get started
+
+For first install the dependencies using npm:
+
+      npm install
+
+Then, run gulp
+
+      gulp
