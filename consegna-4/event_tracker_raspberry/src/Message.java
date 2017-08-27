5deb6fbe37a675cf1882f020ff11d8a2d34972a7
Andrea Colombo <andread251@gmail.com>
2017-08-27 17:38:29 +0200

    Handle Message

---
+++ consegna-4/event_tracker_raspberry/src/Message.java
@@ -0,0 +1,44 @@
+
+public class Message {
+
+	String time;
+	Float temperature;
+	boolean presence;
+	boolean alarm;
+	
+	public Message(String time, Float temperature, boolean presence, boolean alarm) {
+		super();
+		this.time = time;
+		this.temperature = temperature;
+		this.presence = presence;
+		this.alarm = alarm;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public Float getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(Float temperature) {
+		this.temperature = temperature;
+	}
+	public boolean isPresence() {
+		return presence;
+	}
+	public void setPresence(boolean presence) {
+		this.presence = presence;
+	}
+	public boolean isAlarm() {
+		return alarm;
+	}
+	public void setAlarm(boolean alarm) {
+		this.alarm = alarm;
+	}
+	
+	
+	
+}
--- consegna-4/event_tracker_raspberry/src/MessageHandler.java
+++ consegna-4/event_tracker_raspberry/src/MessageHandler.java
@@ -1,9 +1,13 @@
 
+import java.util.Calendar;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.google.gson.Gson;
 
 public class MessageHandler extends Thread {
 	SerialCommChannel channel;
@@ -14,44 +18,52 @@ public class MessageHandler extends Thread {
 	
 	public void run(){
 		
-		while(true){
+		while(true){		
+
+			
 			String msg;
 			try {
 				if(channel.isMsgAvailable()){
+					
 					msg = channel.receiveMsg();
 					
-					post(msg);
-					
 					System.out.println(msg);
+					
+					Gson gson = new Gson();
+					
+					Message message = gson.fromJson(msg, Message.class);
+					
+					Calendar cal = Calendar.getInstance();
+					message.setTime(cal.getTime().toString());
+					
+					post(gson.toJson(message));
+					
 				}
 			} catch (InterruptedException e) {
-				
+				e.printStackTrace();
 			}
 		}
 	}
 	
-	private void post(String msg) {
+	private void post(String message) {
 	
 
-		HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead 
+		HttpClient httpClient = HttpClientBuilder.create().build();
 		
 		try {
 		
 		    HttpPost request = new HttpPost("http://localhost:3000/messages");
-		    StringEntity params =new StringEntity("details={\"time\":\"1503842245\",\"temperature\":\"31.0\",\"presence\":\"false\",\"alarm\":\"false\"} ");
+		    StringEntity params =new StringEntity("details=" + message);
 		    request.addHeader("content-type", "application/json");
 		    request.setEntity(params);
 		    
 		    HttpResponse response = httpClient.execute(request);
 		    System.out.println(response);
 		
-		}catch (Exception ex) {
+		} catch (Exception ex) {
 		
-		    //handle exception here
+		    ex.printStackTrace();
 		
-		} finally {
-		    //Deprecated
-		    //httpClient.getConnectionManager().shutdown(); 
 		}
 		
 	}
